41acbd581ddb1470fd26ab2995af52a44a7428fc
yuta.nishimura <yuta.nishimura@optim.co.jp>
2022-11-23 17:45:54 +0900

    [add] #2 メニューバーのwidgetを追加
    [タスク]ホーム画面を実装する

---
+++ lib/view/BottomBar/bottom_bar.dart
@@ -0,0 +1,65 @@
+import 'package:beer_collection/view/HomePage/home_page.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({Key? key}) : super(key: key);
+  
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int selectedIndex = 0;
+
+  static final List<Widget> widgetOptions = <Widget> [
+    const HomePage(),
+    const Text("Learning"),
+    const Text("Health"),
+    const Text("Profile"),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: widgetOptions[selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+      currentIndex: selectedIndex,
+      onTap: _onItemTapped,
+      elevation: 10,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      selectedItemColor: Colors.blueGrey,
+      type: BottomNavigationBarType.fixed,
+      unselectedItemColor: const Color(0xFF526480),
+      items: const [
+        BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+            label: "Home"),
+        BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_book_formula_database_regular),
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_book_formula_database_filled),
+            label: "Learning"),
+        BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_accessibility_regular),
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_accessibility_filled),
+            label: "Health",
+        ),
+        BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+            label: "Profile"),
+        ],
+      ),
+    );
+  }
+}
