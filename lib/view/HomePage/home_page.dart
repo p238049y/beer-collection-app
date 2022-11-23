2a8ccd14155136f3bc39d29515b82919555e11c0
yuta.nishimura <yuta.nishimura@optim.co.jp>
2022-11-23 17:46:21 +0900

    [add] #2 ホーム画面のwidgetを追加
    [タスク]ホーム画面を実装する

---
+++ lib/view/HomePage/home_page.dart
@@ -0,0 +1,51 @@
+import 'package:beer_collection/util/app_info_first.dart';
+import 'package:beer_collection/util/app_styles.dart';
+import 'package:beer_collection/view/BeerScreen/beer_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: [
+          const Gap(15),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "最新のビール記録",
+                  style: Styles.headLineStyle2,
+                ),
+                InkWell(
+                  onTap: () {
+                    print("You are tapped \"View all\"");
+                  },
+                  child: Text(
+                    "View all > ",
+                    style:
+                        Styles.textStyle.copyWith(color: Styles.primaryColor),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Gap(15),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 20),
+            child: Row(
+              children: beerList.map((beer) => BeerScreen(beer: beer)).toList(),
+            )
+          )
+        ],
+      ),
+    );
+  }
+}
