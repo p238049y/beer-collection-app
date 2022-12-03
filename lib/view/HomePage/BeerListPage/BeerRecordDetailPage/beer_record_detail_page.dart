8546655670cbe81c8a84002687dab2bba54ec328
yuta.nishimura <yuta.nishimura@optim.co.jp>
2022-12-04 01:17:48 +0900

    [add] #15 ビール記録詳細画面の追加
    [タスク] ビール記録の詳細画面を実装する

--- lib/view/HomePage/BeerListPage/BeerRecordDetailPage/beer_record_detail_page.dart
+++ lib/view/HomePage/BeerListPage/BeerRecordDetailPage/beer_record_detail_page.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:beer_collection/widgets/common_back_button_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class BeerRecordDetailPage extends StatefulWidget {
   const BeerRecordDetailPage({Key? key}) : super(key: key);
@@ -11,14 +15,71 @@ class _BeerRecordDetailPageState extends State<BeerRecordDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body : Center(
-        child: TextButton(
-          child: Text("最初のページに戻る"),
-          onPressed: (){
-            Navigator.pop(context);
-          },
-        ),
-      )
+      appBar: const CommonBackButton(),
+      body : Padding(
+        padding: const EdgeInsets.only(right: 16.0, left: 16.0),
+        child: Column(
+        children: [
+          SizedBox(
+            width: 350,
+            height: 250,
+            child: Image.asset('assets/images/tokyo_white.png')
+          ),
+          const Gap(16),
+          const TextField(
+            enabled: false,
+            decoration: InputDecoration(
+              labelText: 'ビール名：東京ホワイト',
+              labelStyle: TextStyle(
+                fontSize: 20,
+                color: Colors.blueGrey,
+                fontWeight: FontWeight.bold,
+              ),
+              border: OutlineInputBorder()
+            ),
+          ),
+          const Gap(16),
+          const TextField(
+            enabled: false,
+            decoration: InputDecoration(
+              labelText: 'ビアスタイル：セゾン',
+              labelStyle: TextStyle(
+                fontSize: 20,
+                color: Colors.blueGrey,
+                fontWeight: FontWeight.bold,
+              ),
+              border: OutlineInputBorder()
+            ),
+          ),
+          const Gap(16),
+          const TextField(
+            enabled: false,
+            decoration: InputDecoration(
+              labelText: 'アルコール度数：5.0%',
+              labelStyle: TextStyle(
+                fontSize: 20,
+                color: Colors.blueGrey,
+                fontWeight: FontWeight.bold,
+              ),
+              border: OutlineInputBorder()
+            ),
+          ),
+          const Gap(16),
+          const TextField(
+            enabled: false,
+            decoration: InputDecoration(
+              labelText: 'カロリー：350kcal',
+              labelStyle: TextStyle(
+                fontSize: 20,
+                color: Colors.blueGrey,
+                fontWeight: FontWeight.bold,
+              ),
+              border: OutlineInputBorder()
+            ),
+          ),
+        ],
+      ),  
+      ),
     );
   }
 }
