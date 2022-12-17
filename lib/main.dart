d2411444d49c3c72241c20555b4228f4075eeb5e
p238049y <p238049y@gmail.com>
2022-12-17 16:17:39 +0900

    [add] #24 多言語化対応を実施
    [タスク] ビール情報をローカルDBに登録する機能を実装する

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:beer_collection/view/BottomBar/bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,8 +19,10 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       localizationsDelegates: const [
+        AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: const [
         Locale("en"),
