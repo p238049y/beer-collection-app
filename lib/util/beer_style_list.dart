9649ca04b71a5ffd8788432e1d8e1ddda67bbf13
p238049y <p238049y@gmail.com>
2022-12-17 16:18:27 +0900

    [add] #14 ビアスタイルのリストを追加
    [タスク] ビール情報をローカルDBに登録する機能を実装する

--- lib/util/beer_style_list.dart
+++ lib/util/beer_style_list.dart
@@ -1,13 +1,7 @@
-import 'package:bs_flutter_selectbox/bs_flutter_selectbox.dart';
-import 'package:flutter/material.dart';
+class BeerStyle {
+  late int id;
+  late String styleName;
+}
 
-BsSelectBoxController beerStyleList = BsSelectBoxController(
-  options: [
-    const BsSelectBoxOption(value: 0, text: Text('')),
-    const BsSelectBoxOption(value: 1, text: Text('IPA')),
-    const BsSelectBoxOption(value: 2, text: Text('セゾン')),
-    const BsSelectBoxOption(value: 3, text: Text('スタウト')),
-    const BsSelectBoxOption(value: 3, text: Text('ピルスナー')),
-    const BsSelectBoxOption(value: 3, text: Text('ヘイジーIPA')),
-  ]
-);
+List<String> beerStyleList = ["IPA", "セゾン", "スタウト", "ピルスナー", "ヘイジーIPA"];
+
