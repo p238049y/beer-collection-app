12bf5dfd85b84e86fb2bb4d480338d0efbb3db2a
p238049y <p238049y@gmail.com>
2022-12-17 20:41:18 +0900

    [update] #25 ビール一覧画面に登録したデータを表示するために、ローカルDBからデータを取得し表示する処理を追加
    [タスク] ビール詳細画面に登録したデータを表示する

--- lib/view/HomePage/BeerListPage/beer_list_page.dart
+++ lib/view/HomePage/BeerListPage/beer_list_page.dart
@@ -1,3 +1,5 @@
+import 'package:beer_collection/entities/beer.dart';
+import 'package:beer_collection/repository/beer/beer.dart';
 import 'package:beer_collection/util/app_info_first.dart';
 import 'package:beer_collection/view/HomePage/BeerListPage/BeerListScreen/beer_list_screen.dart';
 import 'package:beer_collection/widgets/common_back_button_widget.dart';
@@ -11,6 +13,25 @@ class BeerListPage extends StatefulWidget {
 }
 
 class _BeerListPageState extends State<BeerListPage> {
+  List<BeerView> beerList = [];
+
+  Future<void> initDb() async {
+    await DbProvider.setDb();
+    beerList = await DbProvider.getBeerList();
+    setState(() {});
+  }
+
+  Future<void> reBuild() async {
+    beerList = await DbProvider.getBeerList();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initDb();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
