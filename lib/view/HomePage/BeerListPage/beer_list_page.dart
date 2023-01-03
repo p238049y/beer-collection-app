5efd228076a1a3906afab4660e61860b8a3e29a7
p238049y <p238049y@gmail.com>
2023-01-03 15:06:43 +0900

    [update]#38 ビール記録一覧のコンポーネントを親コンポーネントに移植
    [タスク] ビール記録の削除機構を実装する

--- lib/view/HomePage/BeerListPage/beer_list_page.dart
+++ lib/view/HomePage/BeerListPage/beer_list_page.dart
@@ -1,7 +1,9 @@
+import 'dart:io';
+
 import 'package:beer_collection/entities/beer.dart';
 import 'package:beer_collection/repository/beer/beer.dart';
-import 'package:beer_collection/util/app_info_first.dart';
-import 'package:beer_collection/view/HomePage/BeerListPage/BeerListScreen/beer_list_screen.dart';
+import 'package:beer_collection/view/HomePage/BeerListPage/BeerRecordDetailPage/beer_record_detail_page.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:beer_collection/widgets/common_back_button_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -36,7 +38,44 @@ class _BeerListPageState extends State<BeerListPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CommonBackButton(),
-      body : BeerListScreen(beerList: beerList),
+      body :Scaffold(
+        body: ListView.builder(
+      // NOTE: https://qiita.com/code-cutlass/items/3a8b759056db1e8f7639
+      // ListViewやGridViewを入れ子にして、表示やリストのスクロールを可能にするための設定
+      itemCount: beerList.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+            child: Slidable(
+                endActionPane: ActionPane(
+                  extentRatio: 0.25,
+                  motion: const ScrollMotion(),
+                  children: [
+                    SlidableAction(
+                      onPressed: (value) async {
+                        await DbProvider.deleteData(beerList[index].id);
+                        reBuild();
+                      },
+                      backgroundColor: Colors.red,
+                      foregroundColor: Colors.white,
+                      icon: Icons.delete,
+                      label: '削除',
+                    ),
+                  ],
+                ),
+                child: ListTile(
+                  minLeadingWidth: 120.0,
+                  leading: Image.file(File(beerList[index].image)),
+                  title: Text(beerList[index].beerName),
+                  subtitle: Text(beerList[index].registryDateTime),
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) =>
+                          BeerRecordDetailPage(beer: beerList[index]),
+                    ));
+                  },
+                )));
+      },
+    ))
     );
   }
 }
