77514ddf3118305d6df80cca33e39614070a7afc
p238049y <p238049y@gmail.com>
2023-01-05 00:00:19 +0900

    [update]#42 プロフィール登録後に登録したユーザー情報が表示されるようにDBから登録内容を取得する処理を追加
    [タスク] プロフィール画面の実装

--- lib/view/ProfilePage/profile_page.dart
+++ lib/view/ProfilePage/profile_page.dart
@@ -14,7 +14,7 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   List<UserView> userList = [];
-  UserView userDate = UserView(0, '', 0, 0);
+  UserView userDate = UserView(0, '未登録', 0, 0);
 
   Future<void> initDb() async {
     await UserDbProvider.setDb();
@@ -38,53 +38,62 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: userDate.id == 0 ? [
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const ProfileAddPage()));
-            },
-          ),
-        ]: [
-          IconButton(
-            icon: const Icon(Icons.create),
-            onPressed: () {},
-          ),
-        ],
-        iconTheme: const IconThemeData(color: Colors.blueGrey),
-        backgroundColor: Colors.white,
-        elevation: 0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(right: 16.0, left: 16.0),
-        child: Column(
-          children: [
-            SizedBox(
-                width: 350,
-                height: 250,
-                child: Image.asset('assets/images/profile.png')),
-            const Gap(16),
-            ListItem(isEnabled: false, title: '名前', name: userDate.userName),
-            const Gap(16),
-            ListItem(
-                isEnabled: false,
-                title: '身長',
-                name: userDate.height.toString(),
-                unit: 'cm'),
-            const Gap(16),
-            ListItem(
-                isEnabled: false,
-                title: '体重',
-                name: userDate.weight.toString(),
-                unit: 'kg'),
-            const Gap(8),
-            const Text('(他のユーザーに公開されることは一切ありません)',
-                style: TextStyle(fontWeight: FontWeight.bold))
-          ],
+        appBar: AppBar(
+          actions: userDate.id == 0
+              ? [
+                  IconButton(
+                    icon: const Icon(Icons.add),
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const ProfileAddPage()));
+                    },
+                  ),
+                ]
+              : [
+                  IconButton(
+                    icon: const Icon(Icons.create),
+                    onPressed: () {},
+                  ),
+                ],
+          iconTheme: const IconThemeData(color: Colors.blueGrey),
+          backgroundColor: Colors.white,
+          elevation: 0,
         ),
-      ),
-    );
+        body: FutureBuilder(
+          future: reBuild(),
+          builder: (context, snapshot) {
+            return Scaffold(
+              body: Padding(
+                padding: const EdgeInsets.only(right: 16.0, left: 16.0),
+                child: Column(
+                  children: [
+                    SizedBox(
+                        width: 350,
+                        height: 250,
+                        child: Image.asset('assets/images/profile.png')),
+                    const Gap(16),
+                    ListItem(
+                        isEnabled: false, title: '名前', name: userDate.userName),
+                    const Gap(16),
+                    ListItem(
+                        isEnabled: false,
+                        title: '身長',
+                        name: userDate.height.toString(),
+                        unit: 'cm'),
+                    const Gap(16),
+                    ListItem(
+                        isEnabled: false,
+                        title: '体重',
+                        name: userDate.weight.toString(),
+                        unit: 'kg'),
+                    const Gap(8),
+                    const Text('(他のユーザーに公開されることは一切ありません)',
+                        style: TextStyle(fontWeight: FontWeight.bold))
+                  ],
+                ),
+              ),
+            );
+          },
+        ));
   }
 }
