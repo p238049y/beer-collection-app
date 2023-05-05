8a50edf7c06a36f4a64690798ed77aeba4b97891
p238049y <p238049y@gmail.com>
2023-05-05 23:03:38 +0900

    [add] #73 フィットネス画面とそこに使用するモジュールを追加
    [タスク] [v1.1] フィットネス画面の作成

--- lib/view/HealthPage/health_page.dart
+++ lib/view/HealthPage/health_page.dart
@@ -1,4 +1,7 @@
+import 'package:beer_collection/util/get_week_date.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'model.dart';
 
 class HealthPage extends StatelessWidget {
   const HealthPage({Key? key}) : super(key: key);
@@ -6,13 +9,22 @@ class HealthPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body : Center(
-        child: TextButton(
-          child: Text("最初のページに戻る"),
-          onPressed: (){
-            Navigator.pop(context);
-          },
-        ),
+      body : Scaffold(
+        body: ListView.builder(
+          itemCount: healthList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              child: Slidable(
+                child: ListTile(
+                  minLeadingWidth: 120.0,
+                  leading: convertTypeToIcon(healthList[index]['workOutType']),
+                  title: Text(convertDateFormat(healthList[index]['registryDate'], 'yyyy/MM/dd'), maxLines: 1, overflow: TextOverflow.ellipsis),
+                  trailing: Text('${healthList[index]['calorie']}kcal', maxLines: 1, overflow: TextOverflow.ellipsis),
+                ),
+              ),
+            );
+          }
+        )
       )
     );
   }
