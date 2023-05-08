9665a1cd04fcdf71123a86f3afbf1a766dc46ab3
p238049y <p238049y@gmail.com>
2023-05-08 21:08:39 +0900

    [add] #74 登録用のパラメータクラスを追加
    [タスク][v1.1] フィットネス記録画面の作成

--- lib/entities/workout.dart
+++ lib/entities/workout.dart
@@ -6,3 +6,23 @@ class WorkOutView {
 
   WorkOutView(this.id, this.workOutType, this.registryDate, this.calorie);
 }
+
+class RequestWorkOut {
+  int? workOutType;
+  String registryDate;
+  int calorie;
+  double? load;
+  int? frequency;
+  int? time;
+  double? distance;
+
+  RequestWorkOut(
+      {this.workOutType,
+      this.calorie = 0,
+      this.registryDate = '',
+      this.load = 0.0,
+      this.frequency = -1,
+      this.time = -1,
+      this.distance = 0.0
+      });
+}
