445271016f202e96bcd1b9513cf2c4b042c311eb
p238049y <p238049y@gmail.com>
2023-06-27 00:10:32 +0900

    [update]  #83 登録時のバリデーションチェックを追加
    [タスク][v1.1] フィットネス記録をローカルDBに保存する

--- lib/view/HealthPage/WorkOutAddPage/model.dart
+++ lib/view/HealthPage/WorkOutAddPage/model.dart
@@ -1,4 +1,5 @@
 import 'package:beer_collection/entities/workout.dart';
+import 'package:beer_collection/repository/workout/workout.dart';
 
 class RequestWorkOutValidate {
   bool isInValidWorkOutType = false;
@@ -10,70 +11,35 @@ class RequestWorkOutValidate {
   bool isInValidDistance = false;
 
   void checkInputValue(RequestWorkOut registryWorkOut) {
-    if (registryWorkOut.workOutType == null || registryWorkOut.workOutType == -1) {
-      isInValidWorkOutType = true;
-    } else {
-      isInValidWorkOutType = false;
-    }
+    isInValidWorkOutType = (registryWorkOut.workOutType == null ||
+        registryWorkOut.workOutType == -1);
 
-    if (registryWorkOut.registryDate.isEmpty) {
-      isInValidRegistryDate = true;
-    } else {
-      isInValidRegistryDate = false;
-    }
-
-    if (registryWorkOut.load == null || registryWorkOut.load == 0.0) {
-      isInValidLoad = true;
-    } else {
-      isInValidLoad = false;
-    }
-
-    if (registryWorkOut.frequency == null || registryWorkOut.frequency == -1) {
-      isInValidFrequency = true;
-    } else {
-      isInValidFrequency = false;
-    }
-
-    if (registryWorkOut.time == null || registryWorkOut.time == -1) {
-      isInValidTime = true;
-    } else {
-      isInValidTime = false;
-    }
-
-    if (registryWorkOut.distance == null || registryWorkOut.distance == 0.0) {
-      isInValidDistance = true;
-    } else {
-      isInValidDistance = false;
-    }
-
-    if (registryWorkOut.calorie < 1) {
-      isInValidCalorie = true;
-    } else {
-      isInValidCalorie = false;
-    }
+    isInValidRegistryDate = registryWorkOut.registryDate.isEmpty;
+    isInValidLoad =
+        (registryWorkOut.load == null || registryWorkOut.load == 0.0);
+    isInValidFrequency =
+        (registryWorkOut.frequency == null || registryWorkOut.frequency == -1);
+    isInValidTime =
+        (registryWorkOut.time == null || registryWorkOut.time == -1);
+    isInValidDistance =
+        (registryWorkOut.distance == null || registryWorkOut.distance == 0.0);
+    isInValidCalorie = (registryWorkOut.calorie < 1);
   }
 
   bool isInValidData(int workOutType) {
-    switch(workOutType) {
+    switch (workOutType) {
       case 0:
-        return (
-          isInValidWorkOutType ||
-          isInValidRegistryDate ||
-          isInValidTime ||
-          isInValidDistance
-        );
+        return (isInValidWorkOutType ||
+            isInValidRegistryDate ||
+            isInValidTime ||
+            isInValidDistance);
       case 1:
-        return (
-          isInValidWorkOutType ||
-          isInValidRegistryDate ||
-          isInValidLoad ||
-          isInValidFrequency
-        );
+        return (isInValidWorkOutType ||
+            isInValidRegistryDate ||
+            isInValidLoad ||
+            isInValidFrequency);
       case 2:
-        return (
-          isInValidWorkOutType ||
-          isInValidRegistryDate
-        );
+        return (isInValidWorkOutType || isInValidRegistryDate);
       default:
         return true;
     }
