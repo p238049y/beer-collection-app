85580fae9cc6dea815bc1ecd583a5292a2f63154
p238049y <p238049y@gmail.com>
2023-07-07 00:56:11 +0900

    [update]#83 カロリー計算をするメソッドを追加
    [タスク][v1.1] フィットネス記録をローカルDBに保存する

--- lib/view/HealthPage/WorkOutAddPage/model.dart
+++ lib/view/HealthPage/WorkOutAddPage/model.dart
@@ -1,4 +1,6 @@
+import 'package:beer_collection/entities/user.dart';
 import 'package:beer_collection/entities/workout.dart';
+import 'package:beer_collection/repository/user/user.dart';
 import 'package:beer_collection/repository/workout/workout.dart';
 
 class RequestWorkOutValidate {
@@ -48,5 +50,36 @@ class RequestWorkOutValidate {
 
 void registerWorkOut(RequestWorkOut registryWorkOut) async {
   // データ登録の処理
+  double burnedCalorie = 0.0;
+
+  if (registryWorkOut.workOutType == 0) {
+    int elapsedTime = registryWorkOut.time ?? 0;
+    double runningDistance = registryWorkOut.distance ?? 0.0;
+    burnedCalorie =
+        await _calcCaloriesBurnedByRunning(elapsedTime, runningDistance);
+    registryWorkOut.calorie = burnedCalorie;
+  } else if (registryWorkOut.workOutType == 1) {
+    burnedCalorie = await _calcCaloriesBurnedByWeightTraining();
+    registryWorkOut.calorie = burnedCalorie;
+  } else {
+    registryWorkOut.calorie = 0.0;
+  }
+
   await WorkOutDbProvider.insertUserData(registryWorkOut);
+}
+
+Future<double> _calcCaloriesBurnedByRunning(int time, double distance) async {
+  UserView userDate = await UserDbProvider.getUserData();
+  double hour = time / (60 * 60);
+  double speed = distance / hour;
+  double METS = 0.9486 * speed + 0.0931;
+  double calories = userDate.weight * hour * METS * 1.05;
+
+  return calories;
+}
+
+Future<double> _calcCaloriesBurnedByWeightTraining() async {
+  UserView userDate = await UserDbProvider.getUserData();
+  double calories = userDate.weight * 0.05 * 6.0 * 1.05;
+  return calories;
 }
