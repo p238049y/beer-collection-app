1f78d8d9f30f515070d9b148aa5791aa0c0266cc
p238049y <p238049y@gmail.com>
2023-10-04 22:41:20 +0900

    [update]#92 秒数をHH:mm:ssに変換するメソッドを追加
    [タスク][v1.1] ワークアウトの詳細表示機能の追加

--- lib/util/get_week_date.dart
+++ lib/util/get_week_date.dart
@@ -31,3 +31,23 @@ String convertDateFormat(String inputDateTime, String format) {
   DateTime dateTime = outputFormat.parse(inputDateTime);
   return outputFormat.format(dateTime);
 }
+
+// 秒をHH時間mm分ss秒
+String convertDisplayTime(int timeSec) {
+  int hour = 0;
+  int minute = 0;
+  int seconds = 0;
+  if (timeSec < 60) {
+    return '${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')}:${timeSec.toString().padLeft(2, '0')}';
+  } else if (timeSec < 3600) {
+    minute = (timeSec ~/ 60);
+    seconds = timeSec - (60 * minute);
+    return '${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
+  } else {
+    hour = (timeSec ~/ 3600);
+    timeSec = timeSec - (3600 * hour);
+    minute = (timeSec ~/ 60);
+    seconds = timeSec - (60 * minute);
+    return '${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
+  }
+}
